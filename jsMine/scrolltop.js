4d0c627a899eddae7c7c42488236878275ed0254
danocorno <dbodonnell@gmail.com>
2018-01-31 19:12:17 -0600

    Update scrolltop.js

--- jsMine/scrolltop.js
+++ jsMine/scrolltop.js
@@ -1,4 +1,16 @@
 function moveScroller() {
+    var winW = (window.innerWidth) ? window.innerWidth : document.documentElement.offsetWidth;
+    		
+    		if (winW > 640)
+    			{
+                		var sw = "80%";
+    			}
+    		else
+    			{
+    					var sw = "100%";
+    			}			
+    		}	
+    
     var $anchor = $("#scroller-anchor");
     var $scroller = $('#scroller');
 
@@ -8,7 +20,8 @@ function moveScroller() {
         if(st > ot) {
             $scroller.css({
                 position: "fixed",
-                top: "0px"
+                top: "0px",
+                width: sw
             });
         } else {
             $scroller.css({
